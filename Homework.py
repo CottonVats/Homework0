bfec6c53880b203ccd0eb50ba44d6de04ff1cb54
CottonVats <47786883+CottonVats@users.noreply.github.com>
2019-05-09 15:31:46 +0300

    Add files via upload

--- Homework.py
+++ Homework.py
@@ -1,28 +1,38 @@
 import csv
-from pymongo import MongoClient
+import pymongo
 
-client = MongoClient()
+client = pymongo.MongoClient()
+
 homework_db = client['Homework']
 
 
 def read_data(csv_file, db):
     tickets_collection = db['tickets']
+
     with open(csv_file, encoding='utf8') as csvfile:
         reader = csv.DictReader(csvfile, delimiter=',')
+        for row in reader:
+            row['Цена'] = int(row['Цена'])
         tickets_collection.insert_many(reader)
 
 
 def find_cheapest(db):
-    result = db.tickets_collection.find().sort({ 'Цена': -1 })
+    result = db['tickets'].tickets_collection.find().sort('Цена', pymongo.ASCENDING)
+
     return result
 
 
 def find_by_name(name, db):
     all_by_name = db.tickets_collection.find({'Исполнитель': name})
-    sorted_result = all_by_name.sort({ 'Цена': -1 })
+
+    sorted_result = all_by_name.sort('Цена', pymongo.ASCENDING)
+
     return sorted_result
+
 
 if __name__ == '__main__':
     read_data('artists.csv', homework_db)
+
     print(find_cheapest(homework_db))
+
     print(find_by_name('Ария', homework_db))
